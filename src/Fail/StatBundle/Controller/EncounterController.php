ec383962f3fcb29f7d510ff950652ffb8288e846
PsNate <n.river30@gmail.com>
2012-08-12 17:25:23 +0200

    Creating EncounterController

---
+++ src/Fail/StatBundle/Controller/EncounterController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Fail\StatBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Fail\StatBundle\Entity\Event;
+use Fail\StatBundle\Form\EventType;
+use Fail\StatBundle\Entity\Encounter;
+use Fail\StatBundle\Entity\EncounterRepository;
+
+/**
+ * Event controller.
+ *
+ */
+class EncounterController extends Controller
+{
+}
