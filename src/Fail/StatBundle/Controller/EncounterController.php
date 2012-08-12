5f4ae175d90e6634f626ad63865ab7dc1fca8d89
PsNate <n.river30@gmail.com>
2012-08-12 18:43:16 +0200

    Creating the Encounter adding template

--- src/Fail/StatBundle/Controller/EncounterController.php
+++ src/Fail/StatBundle/Controller/EncounterController.php
@@ -7,12 +7,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Fail\StatBundle\Entity\Event;
 use Fail\StatBundle\Form\EventType;
 use Fail\StatBundle\Entity\Encounter;
-use Fail\StatBundle\Entity\EncounterRepository;
-
+use Fail\StatBundle\Form\EncounterType;
 /**
  * Event controller.
  *
  */
 class EncounterController extends Controller
 {
+    public function newEncounterAction()
+    {
+        $encounter = new Encounter;
+        $form = $this->createForm(new EncounterType, $encounter);
+        $argts['form'] = $form->createView();
+        
+        $tpl_path = 'FailStatBundle:Encounter:new.html.twig';
+        return $this->render($tpl_path, $argts);
+    }
 }
