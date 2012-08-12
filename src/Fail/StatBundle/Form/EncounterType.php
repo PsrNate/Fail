0822ead0721934c5f0bb56b6479a791c58e827e3
PsNate <n.river30@gmail.com>
2012-08-12 15:27:44 +0200

    Creating EncounterType class

---
+++ src/Fail/StatBundle/Form/EncounterType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Fail\StatBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+use Doctrine\ORM\EntityRepository;
+
+class EncounterType extends AbstractType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $player_opt = array(
+            'class' => 'FailStatBundle:Encounter',
+            'query_builder' => function(EntityRepository $er) {
+                return $er->createQueryBuilder('p')
+                          ->orderBy('p.name', 'ASC');
+            },
+            'property' => 'name',
+        );
+        
+        $builder->add('winner', 'entity', $player_opt);
+        $builder->add('winner', 'entity', $player_opt);
+        $builder->add('lostRounds', 'integer');
+    }
+
+    public function getName()
+    {
+        return 'fail_statbundle_encountertype';
+    }
+    
+    public function getDefaultOptions()
+    {
+        return array(
+            'data_class' => 'Fail\StatBundle\Entity\Encounter'
+        );
+    }
+}
