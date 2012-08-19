4b8cc9cba6753f07f49113ddb1db885cad27d27e
PsNate <n.river30@gmail.com>
2012-08-19 17:10:04 +0200

    Implementing final index action

--- src/Fail/StatBundle/Controller/DefaultController.php
+++ src/Fail/StatBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Fail\StatBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Fail\StatBundle\Entity\Player;
 
 
 class DefaultController extends Controller
@@ -15,6 +16,11 @@ class DefaultController extends Controller
     
     public function indexAction()
     {
-        return $this->render('FailStatBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getEntityManager();
+        $pr = $em->getRepository('FailStatBundle:Player');
+        $argts['players'] = $pr->findBy(array(), array('elo' => 'desc'));
+        
+        $tpl_path = 'FailStatBundle:Default:index.html.twig';
+        return $this->render($tpl_path, $argts);
     }
 }
