f37fb5b0131aa93c4080866d7b0e1d7e048efdca
PsNate <n.river30@gmail.com>
2012-08-19 22:28:48 +0200

    Adding report generator

--- src/Fail/StatBundle/Controller/DefaultController.php
+++ src/Fail/StatBundle/Controller/DefaultController.php
@@ -28,4 +28,38 @@ class DefaultController extends Controller
         $tpl_path = 'FailStatBundle:Default:index.html.twig';
         return $this->render($tpl_path, $argts);
     }
+    
+    public function reportAction()
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $pr = $em->getRepository('FailStatBundle:Player');
+        $players = $pr->findBy(array(), array('elo' => 'desc'));
+        foreach($players as $p)
+        {
+            $p->setRank($pr->getRank($p));
+            $p->setEx($pr->getEx($p));
+        }
+        
+        $argts['entry'] = '[table][tr]';
+        $argts['entry'] .= '[th]Rang[/th]';
+        $argts['entry'] .= '[th]Nom[/th]';
+        $argts['entry'] .= '[th]Elo[/th]';
+        $argts['entry'] .= '[th]Matches[/th][/tr]';
+        
+        foreach($players as $p)
+        {
+            $argts['entry'] .= '[tr]';
+            $argts['entry'] .= '[td]'.$p->getRank();
+            $argts['entry'] .= $p->getRank() == 1 ? '[sup]er[/sup]' : '[sup]e[/sup]';
+            $argts['entry'] .= $p->getEx() ? ' ex[/td]' : '[/td]';
+            $argts['entry'] .= '[td]'.$p->getName().'[/td]';
+            $argts['entry'] .= '[td]'.$p->getElo().'[/td]';
+            $argts['entry'] .= '[td]'.$e->getMatchesCount().'[/td][/tr]';
+        }
+        
+        $argts['entry'] .= '[/table]';
+        
+        $tpl_path = 'FailStatBundle:Default:report.html.twig';
+        return $this->render($tpl_path, $argts);
+    }
 }
